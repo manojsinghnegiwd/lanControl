8ea48a041dc8cddd9a6833686cb338b24bd8758f
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-29 10:18:46 +0530

    initial setup

--- renderer.js
+++ renderer.js
@@ -1,3 +1,15 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var io = require('socket.io');
+var robot = require("robotjs");
+
+(function () {
+	var start = document.querySelector('#start');
+
+	start.addEventListener('click', move);
+})()
+
+function move () {
+	
+}

b0bf38db1ee06bffc8a407d1db62978dc9f162e4
Manoj Singh Negi <me@manojsinghnegi.com>
2017-04-29 10:53:04 +0530

    added socket io

--- renderer.js
+++ renderer.js
@@ -1,12 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var io = require('socket.io');
+var socket = require('socket.io');
 var robot = require("robotjs");
+var http = require("http");
+var app = http.createServer();
+var io = socket(app);
 
 (function () {
 	var start = document.querySelector('#start');
-
 	start.addEventListener('click', move);
 })();
 
@@ -24,3 +26,9 @@ function move () {
 
 	}, 1)
 }
+
+io.on('connection', (socket) => {
+  console.log('connection');
+})
+
+io.listen(7000);
